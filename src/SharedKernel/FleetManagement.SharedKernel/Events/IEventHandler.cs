d2417ccfa4ac5247619cf30598977b12fe9448ae
lanchemi11 <milan.ristic2002@gmail.com>
2026-06-25 19:35:52 +0200

    Implemented SharedKernel: Entity, Event Bus interfaces and InMemoryEventBus

---
+++ src/SharedKernel/FleetManagement.SharedKernel/Events/IEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManagement.SharedKernel.Events
+{
+    public interface IEventHandler<in TEvent> where TEvent : class, IEvent
+    {
+        Task HandleAsync(TEvent @event);
+    }
+}
---
+++ src/SharedKernel/FleetManagement.SharedKernel/Events/InMemoryEventBus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManagement.SharedKernel.Events
+{
+    public class InMemoryEventBus : IEventBus
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public InMemoryEventBus(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task PublishAsync<TEvent>(TEvent @event) where TEvent : class, IEvent
+        {
+            var handlers = _serviceProvider.GetServices<IEventHandler<TEvent>>();
+
+            foreach (var handler in handlers)
+            {
+                await handler.HandleAsync(@event);
+            }
+        }
+    }
+}
