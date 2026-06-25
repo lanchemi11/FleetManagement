cc52aca34178634e67531835a472013c2f3a6ba0
lanchemi11 <milan.ristic2002@gmail.com>
2026-06-25 20:05:07 +0200

    Temporary commit: added Vehicle entity and ports

---
+++ src/Modules/FleetManagement.Modules.Vehicles/Application/IVehicleRepository.cs
@@ -0,0 +1,17 @@
+﻿using FleetManagement.Modules.Vehicles.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManagement.Modules.Vehicles.Application
+{
+    public interface IVehicleRepository
+    {
+        Task<Vehicle?> GetByIdAsync(Guid id);
+        Task<IEnumerable<Vehicle>> GetAllAsync();
+        Task AddAsync(Vehicle vehicle);
+        Task UpdateAsync(Vehicle vehicle);
+    }
+}
